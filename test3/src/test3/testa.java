55a534b19a8e7c30fff2c97b2ca21a78fdcc94ce
19247 <19247@LAPTOP-2K4UIOC8>
2019-05-30 16:04:10 +0800

    fg

--- test3/src/test3/testa.java
+++ test3/src/test3/testa.java
@@ -4,6 +4,10 @@ public class testa {
 	public static void main(String[] args) {
 		System.out.println("122222222222");
 		System.out.println("qwwwwx");
+		
+		System.out.println("ewrwfwx");
+		
 		System.out.println("fhh");
+
 	}
 }
